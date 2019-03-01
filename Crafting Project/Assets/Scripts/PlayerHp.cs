66827c929b574859a2b821b8ec8f049ec11ad626
Henry Gammad <Henryj4796@yahoo.com>
2019-03-01 15:01:36 -0600

    Adding in my scene and expermenting with damage area
    Added in player controller, color materials, made organization folders and started code for damage area.

---
+++ Crafting Project/Assets/Scripts/PlayerHp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHp : MonoBehaviour
+{
+    public float health = 100.0f;
+
+
+    // Start is called before the first frame update
+    void Start(){
+        health = 100.0f;
+    }
+
+    private void OnTriggerStay(Collider other){
+        if (other.gameObject.tag == "FireArea") {
+            health = health -1;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+        Debug.Log("Player Hp: " + health);
+    }
+
+
+}
