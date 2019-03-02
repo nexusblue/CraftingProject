5132bc4a4026f97a1f88e2c32706004032572728
Henry Gammad <Henryj4796@yahoo.com>
2019-03-01 18:07:36 -0600

    Adjusted health

--- Crafting Project/Assets/Scripts/PlayerHp.cs
+++ Crafting Project/Assets/Scripts/PlayerHp.cs
@@ -6,22 +6,30 @@ public class PlayerHp : MonoBehaviour
 {
     public float health = 100.0f;
 
-
     // Start is called before the first frame update
     void Start(){
         health = 100.0f;
     }
 
-    private void OnTriggerStay(Collider other){
+
+    private void OnTriggerStay(Collider other ){
         if (other.gameObject.tag == "FireArea") {
-            health = health -1;
+            health -= 1 * Time.deltaTime;
+        }
+        if (other.gameObject.tag == "SafeArea" && health < 100 ){
+            health +=  3 * Time.deltaTime;
         }
 
     }
 
+
+
     // Update is called once per frame
     void Update(){
-        Debug.Log("Player Hp: " + health);
+        Debug.Log("Player HP:" + health);
+
+        if (health > 100) { health = 100; }
+
     }
 
 
