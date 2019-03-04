803c0fc88c2622983c24867adefb403fe7faacb5
AMWilkes <amwilkes20@gmail.com>
2019-03-03 19:21:27 -0600

    grab script

---
+++ Crafting Project/Assets/Scripts/grab.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grab : MonoBehaviour
+{
+    public float distance = 5;
+    RaycastHit whatIHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(this.transform.position, this.transform.forward * distance, Color.magenta);
+
+        if (Physics.Raycast(this.transform.position, this.transform.forward, out whatIHit, distance)){
+
+            if (Input.GetKeyDown(KeyCode.E)){
+                Debug.Log("I picked up a " + whatIHit.collider.gameObject.name);
+                //if (whatIHit.collider.tag == "coin"){
+
+                //}
+            }
+        }
+
+    }
+}
