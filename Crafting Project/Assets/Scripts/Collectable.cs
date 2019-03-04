0e351f38b4d3a6324e283e3979ab9ea746f342f0
Henry Gammad <Henryj4796@yahoo.com>
2019-03-03 21:33:04 -0600

    Added new materials and started working on the inventory system

---
+++ Crafting Project/Assets/Scripts/Collectable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+
+    public Inventory.ResourceItem resourceType ;
+
+    private void OnMouseDown()
+    {
+        switch (resourceType) {
+            case Inventory.ResourceItem.WOOD:
+                GameObject.FindGameObjectWithTag("Inventory").GetComponent<Inventory>().wood++;
+                break;
+            case Inventory.ResourceItem.STONE:
+                GameObject.FindGameObjectWithTag("Inventory").GetComponent<Inventory>().stone++;
+                break;
+            case Inventory.ResourceItem.TWINE:
+                GameObject.FindGameObjectWithTag("Inventory").GetComponent<Inventory>().twine++;
+                break;
+
+
+        }
+
+
+    }
+
+
+
+}
---
+++ Crafting Project/Assets/Scripts/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    public enum CraftableItem { SWORD,BOW, PICKAXE  };
+    public enum ResourceItem { WOOD, STONE, TWINE};
+
+    public int wood;
+    public int stone;
+    public int twine;
+
+
+}
