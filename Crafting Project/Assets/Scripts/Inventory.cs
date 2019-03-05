66ad36df11fd500255d1cc0eb49d16948d761c19
Henry Gammad <Henryj4796@yahoo.com>
2019-03-05 13:15:05 -0600

    Fixed out workbench mechanic to turn on and off.

--- Crafting Project/Assets/Scripts/Inventory.cs
+++ Crafting Project/Assets/Scripts/Inventory.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 public class Inventory : MonoBehaviour
 {
 
-    public enum CraftableItem { SWORD,BOW, PICKAXE  };
-    public enum ResourceItem { WOOD, STONE, TWINE};
+    public enum CraftableItem { SWORD, BOW, PICKAXE };
+    public enum ResourceItem { WOOD, STONE, TWINE };
 
     public int wood;
     public int stone;
@@ -17,6 +17,10 @@ public class Inventory : MonoBehaviour
     public Text stoneText;
     public Text twineText;
 
+    public bool hasSword;
+    public bool hasBow;
+    public bool hasPickaxe;
+
     private void Update()
     {
         woodText.text = wood.ToString();
@@ -24,18 +28,50 @@ public class Inventory : MonoBehaviour
         twineText.text = twine.ToString();
     }
 
-    public bool Craft(CraftableItem craft)
-    {
+    //taking button number from UI
+    public void CraftbyInt(int craftInt){
+        if(craftInt == 1) {
+            Craft(CraftableItem.SWORD);
+        }
+        else if (craftInt == 2){
+            Craft(CraftableItem.BOW);
+        }
+        else if (craftInt == 3){
+            Craft(CraftableItem.PICKAXE);
+        }
+
+    }
+
+    public bool Craft(CraftableItem craft){
+
         bool success = false;
         switch (craft) {
             case CraftableItem.SWORD:
+                if (wood>=1 && stone >=2) {
+                    success = true;
+                    wood -= 1;
+                    stone -= 2;
+                    hasSword = true;
+                }
                 break;
             case CraftableItem.BOW:
+                if (wood >= 2 && twine >= 2){
+                    success = true;
+                    wood -= 2;
+                    twine -= 2;
+                    hasBow = true;
+                }
                 break;
             case CraftableItem.PICKAXE:
+                if (wood >= 1 && stone >= 2){
+                    success = true;
+                    wood -= 1;
+                    stone -= 2;
+                    hasPickaxe = true;
+                }
                 break;
         }
-        return false;
+        return success;
 
     }
 
---
+++ Crafting Project/Assets/Scripts/WorkBench.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorkBench : MonoBehaviour{
+
+    public GameObject menuMessage;
+    public GameObject inventScreen;
+    public static bool GameIsPaused = false;
+    
+    // Start is called before the first frame update
+    void Start(){
+        menuMessage.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+    }
+    private void OnTriggerStay(Collider other){
+        if (other.gameObject.tag == "Player"){
+            TurnOnWorkBench();
+        }
+    }
+
+    private void OnTriggerExit(Collider other){
+        if (other.gameObject.tag == "Player"){
+            menuMessage.SetActive(false);
+        }
+
+    }
+
+    // turn on/off workbench menu when f key is pressed
+    private void TurnOnWorkBench(){
+        menuMessage.SetActive(true);
+        if (Input.GetKeyDown(KeyCode.F) && inventScreen.activeSelf == false ){
+            inventScreen.SetActive(true);
+            //Time.timeScale = 0f;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.F) && inventScreen.activeSelf == true){
+            inventScreen.SetActive(false);
+            //Time.timeScale = 1f;
+        }
+
+    }
+
+
+}
